17f428ed9be5b4166eb33fe19abead3c73c6925d
Mehedi Hasan Mubin <mehedi.cseulab201@gmail.com>
2025-06-08 16:33:41 +0600

    implement load task functionality

--- storage.go
+++ storage.go
@@ -2,10 +2,33 @@ package main
 
 import (
 	"encoding/json"
+	"log"
 	"os"
 )
 
 const taskFile = "tasks.json"
+
+func LoadTasks() ([]Task, error) {
+	_, err := os.Stat(taskFile)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return []Task{}, nil
+		}
+	}
+
+	data, err := os.ReadFile(taskFile)
+	if err != nil {
+		return nil, err
+	}
+
+	var loadedTasks []Task
+	err = json.Unmarshal(data, &loadedTasks)
+	if err != nil {
+		return nil, err
+	}
+	
+	return loadedTasks, nil
+}
 
 func SaveTasks(tasks []Task) error {
 	data, err := json.MarshalIndent(tasks, "", "  ")
@@ -19,4 +42,12 @@ func SaveTasks(tasks []Task) error {
 	}
 	
 	return nil
+}
+
+func init() {
+	var err error
+	tasks, err = LoadTasks()
+	if err != nil {
+		log.Fatalf("Error loading tasks: %v\n", err)
+	}
 }
